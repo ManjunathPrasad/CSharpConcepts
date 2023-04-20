8b56a002d7458a577e340a3bddcb454033d3d053
Code Monkey <manjunathprasad99@gmail.com>
2023-04-21 03:22:17 +0530

    Updated Getter and Setter Concept

--- static.Demo/Program.cs
+++ static.Demo/Program.cs
@@ -1,8 +1,11 @@
 ﻿using Static.Demo;
 
-Fish fish = new Fish();
+Fish fish = new Fish("Rohu", "White", "xxl");
+
+Console.WriteLine($"Fish Breed is {fish.FishBreed} \n Fish color is {fish.FishColor} \n Fish size is  {fish.FishSize} ");
 
 Console.WriteLine(fish.CountFish());
 
 /*FishRegion is a static member in class Fish. FishRegion can be accessed using classname without creating object of the class Fish */
 Console.WriteLine(Fish.FishRegion);
+
