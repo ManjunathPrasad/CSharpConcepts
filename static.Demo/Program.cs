0bce4230e9d025c05c5673405ce006fbd84649df
Code Monkey <manjunathprasad99@gmail.com>
2023-04-21 02:23:04 +0530

    Created Fish Class and updated FishRegion as 'static'

--- static.Demo/Program.cs
+++ static.Demo/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Static.Demo;
+
+Fish fish = new Fish();
+
+Console.WriteLine(fish.CountFish());
+
+/*FishRegion is a static member in class Fish. FishRegion can be accessed using classname without creating object of the class Fish */
+Console.WriteLine(Fish.FishRegion);
