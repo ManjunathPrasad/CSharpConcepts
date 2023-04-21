9c79baf31737651b157d2d975ca3bc201fbc14a4
Code Monkey <manjunathprasad99@gmail.com>
2023-04-22 05:23:06 +0530

    Created Engineer class and interface

---
+++ Constructor.Demo/WorksAs/Engineer.cs
@@ -0,0 +1,17 @@
+﻿using Constructor.Demo.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor.Demo.WorksAs
+{
+    public class Engineer : Person, IEngineer
+    {
+        public void GetWorkDuration()
+        {
+            Console.WriteLine(new Random().Next(1, 8));
+        }
+    }
+}
