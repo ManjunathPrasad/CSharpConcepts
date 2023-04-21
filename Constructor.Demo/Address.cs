c6f4d283c218183a39fd7e93f3f542fb4265eaf1
Code Monkey <manjunathprasad99@gmail.com>
2023-04-22 05:22:19 +0530

    Added Address class

---
+++ Constructor.Demo/Address.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor.Demo
+{
+    public class Address
+    {
+        public Address() { }    
+
+        public int HouseNumber { get; set; }
+        public string? StreetName { get; set; }
+        public string? City { get; set; } = null;
+        public string? State { get; set; } = string.Empty;
+        public int PostalCode { get; set; }
+        public string? Country { get; set; } 
+        public int Phone { get; set; }
+    }
+}
