abe99661f28be5bf8e0323afd3e5b6f0546449e3
Code Monkey <manjunathprasad99@gmail.com>
2023-04-22 05:21:30 +0530

    Created Constructor.Demo project

---
+++ Constructor.Demo/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Constructor.Demo.Interfaces;
+
+namespace Constructor.Demo
+{
+    public class Person : IPerson
+    {
+        public static Guid IdentificationNumber { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? FullName { get; set; }
+        public Address? Location { get; set; }
+
+        //Default Constructor
+        public Person()
+        {
+            IdentificationNumber = Guid.NewGuid();
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.Location = Location;
+        }
+
+        //Parameterzed constructor
+        public Person(Guid guid, string firstName, string lastName)
+        {
+            IdentificationNumber = guid;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+
+        //Overloaded Parameterized constructor
+        public Person(string firstName, string lastName, string fullName)
+        {
+            IdentificationNumber = Guid.NewGuid();
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.FullName = fullName;
+        }
+        public void PrintGuid()
+        {
+            Console.WriteLine(IdentificationNumber.ToString());
+        }
+    }
+}
---
+++ Constructor.Demo/Program.cs
@@ -0,0 +1,31 @@
+﻿using Constructor.Demo.WorksAs;
+
+namespace Constructor.Demo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Person Class");
+
+            //Calling Default Constructor
+            Person person = new Person();
+            person.PrintGuid();
+
+            //Calling Parameterized constructor
+            Person emily = new Person(Guid.NewGuid(), "Emily","Bronte");
+            emily.PrintGuid();
+
+            //Overloading Constructor with different parameter types
+            Person ernest = new Person("Ernest", "Hemingway", "Ernest Hemingway");
+            ernest.PrintGuid();
+
+            /*-----------------<Inheritance/interface>----------------*/
+            Engineer Steve = new Engineer();
+            Steve.PrintGuid();
+            Steve.GetWorkDuration();
+            Console.WriteLine(Person.IdentificationNumber);
+
+        }
+    }
+}
