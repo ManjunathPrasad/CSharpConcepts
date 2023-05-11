21b5f088873edae0448631234087854f183651be
Code Monkey <manjunathprasad@gmail.com>
2023-05-11 15:43:52 +0530

    Base keyword demo

---
+++ BaseKeyword.Demo/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace BaseKeyword.Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseClass baseClass = new BaseClass();
+            baseClass.Print();
+
+            DerivedClass derivedClass = new DerivedClass();
+            derivedClass.Print();
+
+            Console.ReadKey();  
+        }
+    }
+}
